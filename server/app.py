e45e48a8701978ae92375465c59665257e0c328c
+ <+>
2023-07-18 16:05:44 -0400

    updated models

--- server/app.py
+++ server/app.py
@@ -3,14 +3,56 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import Flask, request, jsonify
 from flask_restful import Resource
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 # Local imports
 from config import app, db, api
-from models import User, Recipe
+from models import User 
 
 # Views go here!
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@app.route('/register', methods=['POST'])
+def register():
+    # Extract user registration data from request
+    username = request.json['username']
+    password = request.json['password']
+
+    # Check if the username is already taken
+    if User.query.filter_by(username=username).first():
+        return jsonify(message='Username already exists'), 409
+    
+    # Create a new user object and hash the password
+    user = User(username=username)
+    user.password_hash = password
+
+    # Save the user to the database
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(message='User registered successfully')
+
+@app.route('/login', methods=['POST'])
+def login():
+    # Extract user login data from request
+    username = request.json['username']
+    password = request.json['password']
+
+    # Retrieve the user from the database
+    user = User.query.filter_by(username=username).first()
+
+    if user and user.authenticate(password):
+        # Log in the user and store their ID in the session
+        login_user(user)
+        return jsonify(message='Login successful')
+    
+    return jsonify(message='Invalid username or password'), 401
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,94 @@
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import relationship, validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
+from flask_login import UserMixin
 
-from config import db
+from datetime import datetime
 
-# Models go here!
+from config import db, bcrypt
+
+class User(db.Model, SerializerMixin, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True)
+    score = db.Column(db.Integer)
+    house_id = db.Column(db.Integer, db.ForeignKey('houses.id'))
+    house = db.relationship('House', backref='users')
+    _password_hash = db.Column(db.String)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if len(username) > 25 or len(username) < 1:
+            raise ValueError("Username must be between 1 and 25 characters")
+        return username
+    
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+    
+    @property
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        return int(self.id)
+
+    @classmethod
+    def get_by_id(cls, user_id):
+        # Retrieve a user by their ID
+        return cls.query.get(int(user_id))
+
+    @classmethod
+    def get_by_username(cls, username):
+        # Retrieve a user by their username
+        return cls.query.filter_by(username=username).first()
+
+    def __repr__(self):
+        return f"<User id={self.id}, username={self.username}>"
+
+class Ingredient(db.Model, SerializerMixin):
+    __tablename__ = 'ingredients'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(50))
+
+class Potion(db.Model, SerializerMixin):
+    __tablename__ = 'potions'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    correct_ingredients = db.Column(db.ARRAY(db.Integer))
+
+class House(db.Model, SerializerMixin):
+    __tablename__ = 'houses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    scores = db.relationship('Score', backref='house')
+
+class Score(db.Model, SerializerMixin):
+    __tablename__ = 'scores'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', backref='scores')
+    house_id = db.Column(db.Integer, db.ForeignKey('houses.id'))
+    score = db.Column(db.Integer)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
